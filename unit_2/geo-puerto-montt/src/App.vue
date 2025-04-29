688c722d68da00698b7ba675c48382991beef624
AlanSilvaaa <asilvar4@correo.uss.cl>
2025-04-29 14:52:42 -0400

    fix tailwind

--- unit_2/geo-puerto-montt/src/App.vue
+++ unit_2/geo-puerto-montt/src/App.vue
@@ -1,5 +1,11 @@
 <template>
+  <head>
+    <link rel="stylesheet" href="./styles/output.css">
+  </head>
   <img alt="Vue logo" src="./assets/logo.png">
+  <div class="bg-red-500">
+    <p>testing</p>
+  </div>
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
@@ -15,12 +21,12 @@ export default {
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
 </style>
