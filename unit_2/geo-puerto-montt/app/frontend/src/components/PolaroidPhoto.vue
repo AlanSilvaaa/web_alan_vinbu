076d5b0670950977c5235e9ff4fd6a37df3a497b
AlanSilvaaa <asilvar4@correo.uss.cl>
2025-05-12 14:42:54 -0400

    modular polaroid

--- unit_2/geo-puerto-montt/app/frontend/src/components/PolaroidPhoto.vue
+++ unit_2/geo-puerto-montt/app/frontend/src/components/PolaroidPhoto.vue
@@ -1,33 +1,59 @@
 <template>
-  <!-- Card container: preserve 3D context -->
-  <div class="relative w-64 h-80" style="transform-style: preserve-3d;">
+  <!-- Card container -->
+  <div class="relative w-64 h-80" style="transform-style: preserve-3d;" @mouseenter="hoverCard" @mouseleave="unhoverCard" ref="card">
     <!-- Front side -->
     <div class="absolute inset-0 bg-white shadow-lg p-4 flex flex-col justify-between"
       style="backface-visibility: hidden;">
-      <img src="../assets/images/normal_mode_pixel.png" alt="Photo of normal mode"
+      <img :src=front.img alt="Photo of normal mode"
         class="w-full h-64 object-cover mb-4" />
-      <h1 class="text-black font-minecraft">Modo normal</h1>
-      <small class="text-gray-600 text-sm">1855 pts.</small>
+      <h1 class="text-black font-minecraft">{{ front.title }}</h1>
+      <small class="text-gray-600 text-sm">{{ front.highscore }}</small>
     </div>
 
     <!-- Back side -->
     <div class="absolute inset-0 bg-gray-100 shadow-lg p-4 flex flex-col justify-center items-center"
       style="transform: rotateY(180deg); backface-visibility: hidden;">
-      <h2 class="text-black font-minecraft mb-2">Modo normal</h2>
-      <p class="text-gray-700 text-center">Las normas son las siguiente:</p>
+      <h2 class="text-black font-minecraft mb-2">{{ back.title }}</h2>
+      <p class="text-gray-700 text-center">{{ back.description }}</p>
       <ul>
-        <li class="text-gray-700">1. 3 minutos por ronda</li>
-        <li class="text-gray-700">2. 3 rondas</li>
-        <li class="text-gray-700">3. Puedes mover la cámara</li>
+        <li v-for="(rule, index) in back.rules" v-bind:key="index" class="text-gray-700">
+          {{ index + 1 }}. {{ rule }}
+        </li>
       </ul>
     </div>
   </div>
 </template>
 
 <script setup>
-// No extra logic needed here; rotation is handled by parent with GSAP
+import { defineProps, ref } from 'vue';
+import { useSound } from "@vueuse/sound";
+import hover_card from "../assets/sounds/hover_card.wav";
+import gsap from "gsap";
+
+const props = defineProps(["data"]);
+const front = ref(props.data.front);
+const back = ref(props.data.back);
+const card = ref(null);
+
+const { play: hoverCardSound } = useSound(hover_card, { volume: 0.5 });
+
+function hoverCard() {
+  hoverCardSound();
+  gsap.to(card.value, {
+    y: -30,
+    duration: 0.3,
+    ease: "power1.inOut",
+  });
+}
+
+function unhoverCard() {
+  gsap.to(card.value, {
+    y: 0,
+    duration: 0.3,
+    ease: "power1.inOut",
+  });
+}
 </script>
 
 <style scoped>
-/* Optional: smooth edges */
 </style>
